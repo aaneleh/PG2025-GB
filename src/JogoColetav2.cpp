d7491ea525e5831edd02d60bd47d98641c073c6f
aaneleh <helenakurzgon@gmail.com>
2025-06-25 21:45:14 -0300

    funcao move, validacao tile, objetos, e condicao de vitoria

--- src/JogoColetav2.cpp
+++ src/JogoColetav2.cpp
@@ -6,17 +6,12 @@
 
 using namespace std;
 
-// GLAD
 #include <glad/glad.h>
-
-// GLFW
 #include <GLFW/glfw3.h>
 
-// STB_IMAGE
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-//GLM
 #include <glm/glm.hpp> 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -25,7 +20,7 @@ using namespace glm;
 
 #include <fstream>
 
-// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
+//SHADERS
 const GLchar *vertexShaderSource = R"(
 #version 400
 layout (location = 0) in vec3 position;
@@ -40,7 +35,6 @@ gl_Position = projection * model * vec4(position, 1.0);
 }
 )";
 
-// Código fonte do Fragment Shader (em GLSL): ainda hardcoded
 const GLchar *fragmentShaderSource = R"(
 #version 400
 in vec2 tex_coord;
@@ -54,6 +48,7 @@ void main()
 }
 )";
 
+//VARIÁVEIS PERSONAGEM
 struct Player {
 	GLuint VAO;
 	GLuint texID;
@@ -69,8 +64,16 @@ struct Player {
 
 Player personagem;
 
-int setupSprite(int nAnimations, int nFrames, float &ds, float &dt);
+int setupVAOSprite(int nAnimations, int nFrames, float &ds, float &dt);
 
+bool move(int iAnimation, int toTileMapLine, int toTileMapColumn);
+
+//VARIÁVEIS TILEMAP
+int TILEMAP_WIDTH;
+int TILEMAP_HEIGHT;
+
+int t_width;
+int t_height;
 
 struct Tile {
     GLuint VAO;
@@ -81,15 +84,39 @@ struct Tile {
     float ds, dt;
 };
 
-int setupTile(int nTiles, float &ds, float &dt);
+int setupVAOTile(int nTiles, float &ds, float &dt);
 
 vector<Tile> tileset;
 
+vector<int> VALID_TILES;
+
 void desenharMapa(GLuint shaderID);
 
-void desenharItens(GLuint shaderID, GLuint itemTexID);
+void readConfigFile(string filename);
 
-void readMap(string filename);
+float tile_inicial_x; // centro do eixo x - o valor da metade da largura para centralizar o tilemap na janela
+float tile_inicial_y; // divisão da altura da janela pela quantidade de linhas + metade do valor da altura para centralizar o tilemap também no eixo y
+
+vector<int> map;
+
+//VARIÁVEIS ITENS
+struct Collectible {
+	GLuint VAO;
+	GLuint texID;
+	vec3 position;
+	vec3 dimensions;
+	float ds, dt;
+    int tileMapLine;
+    int tileMapColumn;
+};
+
+vector<Collectible> collectibles;
+
+int itens_coletados = 0;
+int total_itens = 0;
+
+void desenharItens(GLuint shaderID);
+
 
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
@@ -98,39 +125,15 @@ int setupShader();
 
 int loadTexture(string filePath, int &width, int &height);
 
-
 const GLuint WIDTH = 1000, HEIGHT = 800;
 
-int TILEMAP_WIDTH = 15;
-int TILEMAP_HEIGHT = 15;
-
-int t_width;
-int t_height;
-
-
-// Matriz de itens: 0 = nada, 1 = item, 2 = obstáculo
-int itens[5][5] = {
-    0, 1, 0, 0, 2,
-    0, 0, 0, 1, 0,
-    1, 0, 2, 0, 0,
-    0, 2, 0, 0, 1,
-    0, 0, 0, 0, 0
-};
-
-int itens_coletados = 0;
-int total_itens = 4;
-
-float tile_inicial_x; // centro do eixo x - o valor da metade da largura para centralizar o tilemap na janela
-float tile_inicial_y; // divisão da altura da janela pela quantidade de linhas + metade do valor da altura para centralizar o tilemap também no eixo y
-
-vector<int> map;
 
 int main(){
 
 	glfwInit();
 	glfwWindowHint(GLFW_SAMPLES, 8);
 	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Jogo coleta - GrauB", nullptr, nullptr);
-	if (!window)	{
+	if (!window){
 		glfwTerminate();
 		return -1;
 	}
@@ -148,27 +151,22 @@ int main(){
 	GLuint shaderID = setupShader();
 	glUseProgram(shaderID);
 
-	readMap("../src/tilemap.txt");
+	readConfigFile("../src/tilemap.txt");
+
 	tile_inicial_x = 400 - t_height; // centro do eixo x - o valor da metade da largura para centralizar o tilemap na janela
 	tile_inicial_y = (600 / TILEMAP_HEIGHT) + t_height/2; // divisão da altura da janela pela quantidade de linhas + metade do valor da altura para centralizar o tilemap também no eixo y
 	
-	std::cout << "X: " << tile_inicial_x << std::endl;
-	std::cout << "Y: " << tile_inicial_y << std::endl;
 	//CARREGANDO PERSONAGEM
 	int imgWidth, imgHeight;
 	personagem.nAnimations = 4;
 	personagem.nFrames = 10;
-	personagem.VAO = setupSprite(personagem.nAnimations,personagem.nFrames,personagem.ds,personagem.dt);
+	personagem.VAO = setupVAOSprite(personagem.nAnimations,personagem.nFrames,personagem.ds,personagem.dt);
 	personagem.position = vec3(0, 0, 1.0);
 	personagem.dimensions = vec3(35, 35, 1.0);
 	GLuint personagemID = loadTexture("../assets/sprites/sprite_full.png",imgWidth,imgHeight);
 	personagem.texID = personagemID;
 	personagem.iAnimation = 1;
 	personagem.iFrame = 0;
-    
-	//CARRENGADO TILESET
-	
-
 
 	//ATIVANDO TEXTURA
 	glActiveTexture(GL_TEXTURE0);
@@ -178,94 +176,49 @@ int main(){
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-
 	//MATRIZ DE PROJEÇÃO
 	mat4 projection = ortho(0.0, 800.0, 0.0, 600.0, -1.0, 1.0);
 	glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
-
 
 	double lastTime = 0.0;
 	double deltaT = 0.0;
 	double currTime = glfwGetTime();
 	double FPS = 12.0;
 
-	total_itens = 0;
-	for (int i = 0; i < TILEMAP_HEIGHT; i++)
-		for (int j = 0; j < TILEMAP_WIDTH; j++)
-			if (itens[i][j] == 1) total_itens++;
-
 	bool venceu = false;
-	
+
 	while (!glfwWindowShouldClose(window))	{
 		
 		glfwPollEvents();
 
-		glClearColor(0.54f, 0.77f, 0.98f, 1.0f); // cor de fundo
+		glClearColor(0.54f, 0.77f, 0.98f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glLineWidth(10);
 		glPointSize(20);
 
-        desenharMapa(shaderID);
-
-        mat4 model = mat4(1);
-		currTime = glfwGetTime();
-		deltaT = currTime - lastTime;
-		
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-        float x = 0;
-        float y = 0;
-
-        if(personagem.tileMapLine > TILEMAP_HEIGHT){
-            personagem.tileMapLine = TILEMAP_HEIGHT;
-        } 
-        if(personagem.tileMapLine < 1){
-            personagem.tileMapLine = 1;
-        } 
-        if(personagem.tileMapColumn > TILEMAP_WIDTH){
-            personagem.tileMapColumn = TILEMAP_WIDTH;
+		if (!venceu && itens_coletados == total_itens && map[personagem.tileMapLine-1 + ((personagem.tileMapColumn-1)*TILEMAP_HEIGHT)] == 6 ) {
+            venceu = true;
+            std::cout << "Voce venceu! Todos os itens coletados." << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            glfwSetWindowShouldClose(window, GL_TRUE);
         }
-        if(personagem.tileMapColumn < 1){
-            personagem.tileMapColumn = 1;
-        } 
 
-        x = tile_inicial_x + t_height + (personagem.tileMapColumn - personagem.tileMapLine) * t_height;
-        y = tile_inicial_y + (personagem.tileMapColumn + personagem.tileMapLine) * (t_height/2);
-
-        personagem.position.x = x;
-        personagem.position.y = y;
-
-		// Lógica de coleta
-		int lin = 1;
-		int col = 1;
-
-		if (lin >= 0 && lin < TILEMAP_HEIGHT && col >= 0 && col < TILEMAP_WIDTH){
-			if (map[lin + col * TILEMAP_HEIGHT] == 4) {
-				for (int i = 0; i < TILEMAP_HEIGHT; i++)
-					for (int j = 0; j < TILEMAP_WIDTH; j++)
-						if (map[i + j * TILEMAP_HEIGHT] == 4) map[i + j * TILEMAP_HEIGHT] = 1;
-				std::cout << "Todos os tiles 4 foram trocados por 1." << std::endl;
-			}
-		}
-
-		if (!venceu && (itens_coletados == total_itens || map[lin + col * TILEMAP_HEIGHT] == 9)) {
-    		venceu = true;
-			if (itens_coletados == total_itens)
-				std::cout << "Você venceu! Todos os itens coletados." << std::endl;
-			else
-				std::cout << "Você venceu ao chegar no tile objetivo!" << std::endl;
-			std::this_thread::sleep_for(std::chrono::seconds(2));
-			glfwSetWindowShouldClose(window, GL_TRUE);
-		}
+        desenharMapa(shaderID);
+		desenharItens(shaderID);
 
 		// Desenho do personagem
+        personagem.position.x = tile_inicial_x + t_height + (personagem.tileMapColumn - personagem.tileMapLine) * t_height;
+        personagem.position.y = tile_inicial_y + (personagem.tileMapColumn + personagem.tileMapLine) * (t_height/2);
+        mat4 model = mat4(1);
 		model = mat4(1);
 		model = translate(model, personagem.position);
 		model = rotate(model, radians(0.0f), vec3(0.0, 0.0, 1.0));
 		model = scale(model,personagem.dimensions);
 		glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model));
 
+		currTime = glfwGetTime();
+		deltaT = currTime - lastTime;
 		vec2 offsetTex;
 		
 		if (deltaT >= 1.0/FPS){
@@ -292,59 +245,75 @@ int main(){
 }
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode){
-	if (action == GLFW_PRESS) {
-        std::cout << "[DEBUG] Tecla pressionada: " << key << std::endl;
-    }
+
 	personagem.isWalking = true;
 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
-
 	if (action != GLFW_PRESS && action != GLFW_REPEAT){
 		personagem.isWalking = false;
-	} 
+	}
 	
-    if (key == GLFW_KEY_LEFT && action == GLFW_PRESS){
-        personagem.iAnimation = 2;
-        personagem.tileMapColumn -= 1;
-    }
-    if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS){
-        personagem.iAnimation = 4;
-        personagem.tileMapColumn += 1;
-    }
-    if (key == GLFW_KEY_UP && action == GLFW_PRESS){
-        personagem.iAnimation = 3;
-        personagem.tileMapLine += 1;
-    }
-    if (key == GLFW_KEY_DOWN && action == GLFW_PRESS){
-        personagem.iAnimation = 1;
-        personagem.tileMapLine -= 1;
-    }	
-
-    if (key == GLFW_KEY_A && action == GLFW_PRESS){
-        personagem.iAnimation = 2;
-        personagem.tileMapLine += 1;
-        personagem.tileMapColumn -= 1;
-    }
-    if (key == GLFW_KEY_D && action == GLFW_PRESS){
-        personagem.iAnimation = 4;
-        personagem.tileMapLine -= 1;
-        personagem.tileMapColumn += 1;
-    }
-    if (key == GLFW_KEY_W && action == GLFW_PRESS){
-        personagem.iAnimation = 3;
-        personagem.tileMapLine += 1;
-        personagem.tileMapColumn += 1;
-    }
-    if (key == GLFW_KEY_S && action == GLFW_PRESS){
-        personagem.iAnimation = 1;
-        personagem.tileMapLine -= 1;
-        personagem.tileMapColumn -= 1;
-    }
+	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS){
+		move(2, personagem.tileMapLine, personagem.tileMapColumn-1);
+    
+	} else if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS){
+		move(4, personagem.tileMapLine, personagem.tileMapColumn+1);
+    
+	} else if (key == GLFW_KEY_UP && action == GLFW_PRESS){
+		move(3, personagem.tileMapLine+1, personagem.tileMapColumn);
+		
+	} else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS){
+		move(1, personagem.tileMapLine-1, personagem.tileMapColumn);
+    
+	} else if (key == GLFW_KEY_A && action == GLFW_PRESS){
+		move(2, personagem.tileMapLine+1, personagem.tileMapColumn-1);
+    
+	} else if (key == GLFW_KEY_D && action == GLFW_PRESS){
+		move(4, personagem.tileMapLine-1, personagem.tileMapColumn+1);
+    
+	} else if (key == GLFW_KEY_W && action == GLFW_PRESS){
+		move(3, personagem.tileMapLine+1, personagem.tileMapColumn+1);
+    
+	} else if (key == GLFW_KEY_S && action == GLFW_PRESS){
+		move(1, personagem.tileMapLine-1, personagem.tileMapColumn-1);
+    } 
 }
 
-void readMap(string filename) {
+bool move(int iAnimation, int toTileMapLine, int toTileMapColumn){
+
+	for(int i = 0; i < VALID_TILES.size(); i++){
+		if(VALID_TILES[i] == map[toTileMapLine-1 + ((toTileMapColumn-1)*TILEMAP_HEIGHT)]){
+			personagem.iAnimation = iAnimation;
+
+			//NÃO DEIXA O PERSONAGEM SAIR DO MAPA
+			personagem.tileMapLine = fmax(1, fmin(toTileMapLine, TILEMAP_WIDTH));
+			personagem.tileMapColumn = fmax(1, fmin(toTileMapColumn, TILEMAP_HEIGHT));
+
+			//MUDA TILE DE PEDRA (2) PARA LAVA (3)
+			if(map[toTileMapLine-1 + ((toTileMapColumn-1)*TILEMAP_HEIGHT)] == 2) map[toTileMapLine-1 + ((toTileMapColumn-1)*TILEMAP_HEIGHT)] = 3;
+
+			//VERIFICA COLETA DE ITENS
+			for (int i = 0; i < collectibles.size(); i++) {
+				if (personagem.tileMapLine == collectibles[i].tileMapLine && personagem.tileMapColumn == collectibles[i].tileMapColumn) {
+					collectibles.erase(collectibles.begin() + i); // Remove o item coletado da lista
+					itens_coletados++;
+					if(itens_coletados == total_itens)
+						std::cout << "Agora vá até o tile objetivo para vencer!" << std::endl;
+			
+					std::cout << "Coletou um item! Total: " << itens_coletados << std::endl;
+					break; // Importante para evitar acessar como índice inválido
+				}
+			}
+
+			return true;
+		}
+	}
+	return false;
+}
+
+void readConfigFile(string filename) {
 	ifstream file(filename);
 
 	//NOME DO ARQUIVO DE TEXTURA
@@ -371,7 +340,7 @@ void readMap(string filename) {
         tile.dimensions = vec3(stoi(tile_width), stoi(tile_height), 1.0);
         tile.iTile = i;
         tile.texID = texID;
-        tile.VAO = setupTile( stoi(num_tiles), tile.ds, tile.dt);
+        tile.VAO = setupVAOTile( stoi(num_tiles), tile.ds, tile.dt);
         tileset.push_back(tile);
     }
 
@@ -380,8 +349,7 @@ void readMap(string filename) {
 	file >> tilemap_h;    
 	file >> tilemap_w;   
 	TILEMAP_HEIGHT = stoi(tilemap_h);
-	TILEMAP_HEIGHT = stoi(tilemap_w);
-
+	TILEMAP_WIDTH = stoi(tilemap_w);
 
 	//CRIA O TILE MAP
 	string type;
@@ -391,7 +359,6 @@ void readMap(string filename) {
 	} 
 
 	//ADICIONA TILES 'CAMINHÁVEIS' PRA CONSTANTE 'VALID_TILES'
-	/* 
 	string currentEl;
 	do {
 		file >> currentEl;
@@ -401,11 +368,11 @@ void readMap(string filename) {
 
 
 	//CRIA OS COLETÁVEIS
-	collectible collectibleEl;
+	Collectible collectibleEl;
 	string textureCollectible;
 	file >> textureCollectible;
 	collectibleEl.texID = loadTexture("../assets/" + textureCollectible, imgWidth, imgHeight);
-	collectibleEl.VAO = createVAOPlayer(1, 1, collectibleEl.ds, collectibleEl.dt);
+	collectibleEl.VAO = setupVAOSprite(1, 1, collectibleEl.ds, collectibleEl.dt);
 
 	string posX, posY;
 	while(file >> posX){ 
@@ -413,13 +380,13 @@ void readMap(string filename) {
 		collectibleEl.tileMapLine = stoi(posX);
 		collectibleEl.tileMapColumn = stoi(posY);
 		collectibles.push_back(collectibleEl);
-		//cout << ">>TILELINE: " << collectibleEl.tileMapLine << "\nTILECOLUMN: " << collectibleEl.tileMapColumn << endl;
-	}  */
+		total_itens++;
+	} 
 
 	file.close();
 }
 
-int setupSprite(int nAnimations, int nFrames, float &ds, float &dt){
+int setupVAOSprite(int nAnimations, int nFrames, float &ds, float &dt){
 
 	ds = 1.0 / (float) nFrames;
 	dt = 1.0 / (float) nAnimations;
@@ -454,8 +421,7 @@ int setupSprite(int nAnimations, int nFrames, float &ds, float &dt){
 	return VAO;
 }
 
-int setupTile(int nTiles, float &ds, float &dt){
-
+int setupVAOTile(int nTiles, float &ds, float &dt){
     ds = 1.0 / (float)nTiles;
     dt = 1.0;
 
@@ -491,35 +457,48 @@ int setupTile(int nTiles, float &ds, float &dt){
 }
 
 void desenharMapa(GLuint shaderID){
-    // dá pra fazer um cálculo usando tilemap_width e tilemap_height
-    // float x0 = 400;
-    // float y0 = 100;
-
     for (int i = 0; i < TILEMAP_HEIGHT; i++){
         for (int j = 0; j < TILEMAP_WIDTH; j++){
-            // Matriz de transformaçao do objeto - Matriz de modelo
-            mat4 model = mat4(1); // matriz identidade
-
-            Tile curr_tile = tileset[map[i + j * TILEMAP_HEIGHT]];
-
+			Tile curr_tile = tileset[map[i + j * TILEMAP_HEIGHT]];
+			
             float x = tile_inicial_x + (j - i) * curr_tile.dimensions.x/2.0;
             float y = tile_inicial_y + (i + j) * curr_tile.dimensions.y/2.0;
-        
+			
+			mat4 model = mat4(1);
             model = translate(model, vec3(x, y, 0.0));
             model = scale(model, curr_tile.dimensions);
             glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model));
 
             vec2 offsetTex;
-
             offsetTex.s = curr_tile.iTile * curr_tile.ds;
             offsetTex.t = 0.0;
             glUniform2f(glGetUniformLocation(shaderID, "offsetTex"), offsetTex.s, offsetTex.t);
-
             glBindVertexArray(curr_tile.VAO);              
             glBindTexture(GL_TEXTURE_2D, curr_tile.texID); 
-
             glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
         }
+    }
+}
+
+void desenharItens(GLuint shaderID){
+    for (int i = 0; i < collectibles.size(); i++){
+			Collectible curr_collectible = collectibles[i];
+			
+			float x = tile_inicial_x + t_height + (collectibles[i].tileMapColumn - collectibles[i].tileMapLine) * t_height;
+			float y = tile_inicial_y + (collectibles[i].tileMapColumn + collectibles[i].tileMapLine) * (t_height/2);
+
+			mat4 model = mat4(1);
+			model = translate(model,vec3(x,y,0.0));
+            model = scale(model, vec3(20,20,0.0));
+            glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model));
+
+            vec2 offsetTex;
+            offsetTex.s = 1.0;
+            offsetTex.t = 1.0;
+            glUniform2f(glGetUniformLocation(shaderID, "offsetTex"), offsetTex.s, offsetTex.t);
+            glBindVertexArray(curr_collectible.VAO);              
+            glBindTexture(GL_TEXTURE_2D, curr_collectible.texID); 
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     }
 }
 
